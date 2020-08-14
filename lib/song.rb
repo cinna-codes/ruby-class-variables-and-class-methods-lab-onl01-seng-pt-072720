06ac8e2ad3ff68f26f614915231832eba8de9f3b
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-14 16:33:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ class Song
 
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@artists = 0
-  @@genres = 0
+  @@artists = []
+  @@genres = []
 
   def initialize(song_name, song_artist, song_genre)
     @@song_count += 1
